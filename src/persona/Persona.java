3ab0f7a9587bb8290ea05ebbbf410ea70fe4d982
Dany Joseph Juarez Romero <orange_555321@hotmail.com>
2025-06-26 10:12:47 -0600

    Día 1 - Clase Persona, atributos y método mostrar()

---
+++ src/persona/Persona.java
@@ -0,0 +1,14 @@
+package persona;
+
+public class Persona {
+    String nombre;
+    int edad;
+    String ciudad;
+
+
+    public Persona(String nombre, int edad, String ciudad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ciudad = ciudad;
+    }
+}
