b6df115fe6e71c2a54d1bc16f28297c2293d39c3
Dany Joseph Juarez Romero <orange_555321@hotmail.com>
2025-06-27 10:14:21 -0600

    Día 2 - Reto 1: Entrada desde consola y creación dinámica de Persona

--- src/persona/Main.java
+++ src/persona/Main.java
@@ -1,21 +1,39 @@
 package persona;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        //System.out.println("Hello and welcome!");
+        ArrayList <Persona> personas = new ArrayList<>();
+        //personas.add(new Persona("Dany", 32, "Puebla"));
+        //personas.add(new Persona("Luis", 27, "CDMX"));
+        //Persona persona1 = new Persona("Dany", 32, "Puebla");
+        Scanner input = new Scanner(System.in);
 
-        /*for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }*/
-        Persona persona1 = new Persona("Dany", 32, "Puebla");
-        System.out.println("Datos de la Persona");
-        persona1.mostrar();
+        System.out.println("Ingresa Username");
+        String username = input.nextLine();
+
+        System.out.println("Ingresa edad");
+        int edad = input.nextInt();
+        String salto = input.nextLine();
+
+        System.out.println("Ingresa ciudad");
+        String ciudad = input.nextLine();
+
+        personas.add(new Persona(username, edad, ciudad));
+
+        /*System.out.println("Nombre: " +username);
+        System.out.println("Edad: " +edad);
+        System.out.println("ciudad: " +ciudad);*/
+
+        //System.out.println("Datos de la Persona");
+        //persona1.mostrar();
+        System.out.println("--------------------");
+        for (int i=0; i < personas.size(); i++) {
+            personas.get(i).mostrar();
+            System.out.println("-----------------");
+        }
+
 
     }
 }
